2483533a0f9b70664a9753235e22c6b69a7dbb0b
Danil Nikolaev <lischenkodev@gmail.com>
2026-01-22 14:43:32 +0300

    update leave command requirements

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -8,7 +8,12 @@ export class Leave extends ChatCommand {
     title = "/leave";
     description = "Bot will leave current chat";
 
-    requirements = Requirements.Build(Requirement.BOT_ADMIN, Requirement.CHAT);
+    requirements = Requirements.Build(
+        Requirement.BOT_ADMIN,
+        Requirement.CHAT,
+        Requirement.CHAT_ADMIN,
+        Requirement.BOT_CHAT_ADMIN
+    );
 
     async execute(msg: Message): Promise<void> {
         await bot.leaveChat({chat_id: msg.chat.id});
